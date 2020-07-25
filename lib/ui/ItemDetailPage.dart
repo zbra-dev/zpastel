c07d0444cd65ae3c4294769487e15cc09fe0a182
José Luz <jluz2@slb.com>
2020-07-25 11:20:44 -0300

    Included bottom bar in ItemDetail and Sent button

--- lib/ui/ItemDetailPage.dart
+++ lib/ui/ItemDetailPage.dart
@@ -25,42 +25,68 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarBuilder().withTitle("Detalhes do item").withClose().build(),
-      body: SingleChildScrollView(
-        physics: ScrollPhysics(),
-        child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-          child: Column(
-            children: <Widget>[
-              Align(alignment: Alignment.topLeft, child: Text(_flavor.name, maxLines: 1, style: AppTextTheme.of(context).textLarge)),
-              SizedBox(height: 15),
-              Align(alignment: Alignment.topLeft, child: Text(_flavor.ingredients, style: AppTextTheme.of(context).textDefault.copyWith(color: AppColors.secondaryTextColor))),
-              SizedBox(height: 15),
-              Align(alignment: Alignment.topLeft, child: Text(_flavor.valueInReal, maxLines: 1, style: AppTextTheme.of(context).textDefault)),
-              SizedBox(height: 20),
-              TextFormField(
-                keyboardType: TextInputType.text,
-                onChanged: (value) {
-                  _extraInformation = value;
-                },
-                maxLines: null,
-                style: TextStyle(fontSize: 16),
-                validator: (value) {
-                  if (value.isEmpty || value.length < 1) {
-                    return 'Adicione um comentário';
-                  }
-                  return null;
-                },
-                decoration: new InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintStyle: TextStyle(fontSize: 16),
-                  labelText: 'Alguma observação?',
-                  labelStyle: new TextStyle(decorationStyle: TextDecorationStyle.solid),
+      body: Stack(children: <Widget>[
+        SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: <Widget>[
+                Align(alignment: Alignment.topLeft, child: Text(_flavor.name, maxLines: 1, style: AppTextTheme.of(context).textLarge)),
+                SizedBox(height: 15),
+                Align(alignment: Alignment.topLeft, child: Text(_flavor.ingredients, style: AppTextTheme.of(context).textDefault.copyWith(color: AppColors.secondaryTextColor))),
+                SizedBox(height: 15),
+                Align(alignment: Alignment.topLeft, child: Text(_flavor.valueInReal, maxLines: 1, style: AppTextTheme.of(context).textDefault)),
+                SizedBox(height: 20),
+                TextField(
+                  keyboardType: TextInputType.text,
+                  onChanged: (value) {
+                    _extraInformation = value;
+                  },
+                  minLines: 3,
+                  maxLines: null,
+                  autocorrect: true,
+                  textAlignVertical: TextAlignVertical.top,
+                  style: TextStyle(fontSize: 16),
+                  decoration: new InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintStyle: TextStyle(fontSize: 16),
+                    labelText: 'Alguma observação?',
+                    labelStyle: new TextStyle(decorationStyle: TextDecorationStyle.solid),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
-      ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.gray7,
+                  blurRadius: 20.0,
+                  offset: Offset(0, -5.0),
+                )
+              ],
+            ),
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+            child: Row(
+              children: <Widget>[
+                FlatButton(
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 16),
+                  color: AppColors.primaryColor,
+                  shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(10)),
+                  onPressed: () {},
+                  child: Text("Enviar", style: AppTextTheme.of(context).textDefaultBold.copyWith(color: AppColors.negativeTextColor)),
+                )
+              ],
+            ),
+          ),
+        )
+      ]),
     );
   }
 }
