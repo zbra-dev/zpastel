5d14b79bd3844c7c9300cf371224113c22266ea0
Jayme Preto <jayme-arthur@hotmail.com>
2020-07-26 03:02:44 -0300

    - Added flavor to Item;

--- lib/model/Item.dart
+++ lib/model/Item.dart
@@ -1,7 +1,10 @@
+import 'package:zpastel/model/Flavor.dart';
+
 import 'StorableEntity.dart';
 
 abstract class Item implements StorableEntity {
   String orderId;
   int qtdy;
+  Flavor flavor;
 }
 
